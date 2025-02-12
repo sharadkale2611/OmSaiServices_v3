c269f42457c81eb41a4d6542d67cd15ea6dd71c3
sharadkale2611 <sharadkale2611@gmail.com>
2025-02-12 19:52:06 +0530

    Site-list api completed

---
+++ MVC/Areas/Worker/Controllers/ApiSiteController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OmSaiModels.Common;
+using OmSaiModels.Worker;
+using OmSaiServices.Admin.Implementations;
+
+namespace OmSaiServices_v3.Areas.Worker.Controllers
+{
+	[Route("api/site")]
+	[ApiController]
+	public class ApiSiteController : ControllerBase
+	{
+		private readonly SiteService _siteService;
+
+		public ApiSiteController()
+		{
+			_siteService = new SiteService();
+		}
+
+
+		[HttpGet("/")]
+		[Authorize(AuthenticationSchemes = "Jwt")]
+		public async Task<IActionResult> GetSites()
+		{
+			try
+			{
+				var sites =  _siteService.GetAll();
+				if (sites == null)
+				{
+					var errors = new
+					{
+						WorkmanId = new[] { "Sites not found!" }
+					};
+					return BadRequest(new ApiResponseModel<object>(false, null, errors));
+				}
+
+				return Ok(new ApiResponseModel<object>(true, sites, null));
+
+			}
+			catch (Exception ex)
+			{
+				var errors = new
+				{
+					message = new[] { ex.Message }
+				};
+
+				return BadRequest(new ApiResponseModel<object>(false, null, errors));
+
+			}
+
+		}
+
+
+	}
+}
